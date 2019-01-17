052cae69a30e4cf761b60cb79bc31b86ef8b86f6
Daniil Lebedinsky <daniil.lebedinskii@gmail.com>
2019-01-17 17:11:22 +0300

    Add Express.js base application

---
+++ express/index.js
@@ -0,0 +1,47 @@
+const http = require('http');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const { NotFoundError } = require('./lib/errors');
+
+(async () => {
+    const host = process.env.APP_HOST || 'localhost';
+    const port = process.env.APP_PORT || 8080;
+
+    const app = express();
+
+    app.use(bodyParser.json());
+
+    app.use(
+        methodOverride('_method'),
+        methodOverride('X-HTTP-Method'),
+        methodOverride('X-HTTP-Method-Override'),
+        methodOverride('X-Method-Override'),
+    );
+
+    app.use(require('./routes'));
+
+    app.use(
+        (req, res, next) => next(new NotFoundError('route not found')),
+        (err, req, res, next) => {
+            console.log(err);
+            res.status(err.status || 502);
+            res.send(err);
+        }
+    );
+
+    const server = http.createServer(app)
+        .listen(
+            port,
+            host,
+            () => console.log(`Worker ${process.pid} started ${host}:${port}`)
+        );
+
+    server.on('connection', socket => socket.setNoDelay());
+})()
+    .catch((e) => {
+        console.log(e);
+        process.exit(1);
+    });
